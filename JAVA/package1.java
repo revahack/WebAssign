fb85e66397266275472b6e5d7e38ff4a0c032efd
Shivendra Saurav <shivendrasaurav@gmail.com>
2021-08-12 11:51:34 +0530

    static package

--- JAVA/package1.java
+++ JAVA/package1.java
@@ -1,7 +1,19 @@
 package package1;
 
+class A{
+    static int y=100;
+    public static void disp(){
+        System.out.println(y);
+    }
+}
+
 public class package1 {
     public static void main(String args) {
-        System.out.println("Hi " + args + " ! From a Package!!");
+        System.out.println("Hi " + args + " ! From a Package!!\n\n");
+        A a = new A();
+        System.out.println(a.y);
+        a.y=200;
+        System.out.println(a.y);
+        A.disp();
     }
 }
